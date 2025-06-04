51ff0b738421812d5d748d18b94e4279aaacaad6
Quang Vinh Dang <dqvinh101@gmail.com>
2025-06-03 20:42:38 -0700

    Starting on wifi module, adding load .env to the project

---
+++ main/inc/mod_wifi.h
@@ -0,0 +1,6 @@
+#ifndef __MOD_WIFI_H__
+#define __MOD_WIFI_H__
+
+void mod_wifi_init(void);
+
+#endif //__MOD_WIFI_H__
--- main/main.c
+++ main/main.c
@@ -7,6 +7,7 @@
 #include "esp_log.h"
 #include "freertos/task.h"
 #include "lvgl_display.h"
+#include "mod_wifi.h"
 #include "touch_controller.h"
 #include "ui.h"
 #include <stdio.h>
@@ -24,26 +25,26 @@ static void example_lvgl_port_task(void *arg) {
   ESP_LOGI(TAG, "Starting LVGL task");
   uint32_t time_till_next_ms = 0;
   while (1) {
-	_lock_acquire(&lvgl_api_lock);
-	time_till_next_ms = lv_timer_handler();
-	_lock_release(&lvgl_api_lock);
-	// in case of triggering a task watch dog time out
-	time_till_next_ms = MAX(time_till_next_ms, LVGL_TASK_MIN_DELAY_MS);
-	// in case of lvgl display not ready yet
-	time_till_next_ms = MIN(time_till_next_ms, LVGL_TASK_MAX_DELAY_MS);
-	usleep(1000 * time_till_next_ms);
+    _lock_acquire(&lvgl_api_lock);
+    time_till_next_ms = lv_timer_handler();
+    _lock_release(&lvgl_api_lock);
+    // in case of triggering a task watch dog time out
+    time_till_next_ms = MAX(time_till_next_ms, LVGL_TASK_MIN_DELAY_MS);
+    // in case of lvgl display not ready yet
+    time_till_next_ms = MIN(time_till_next_ms, LVGL_TASK_MAX_DELAY_MS);
+    usleep(1000 * time_till_next_ms);
   }
 }
 
 // Task to update time every second
 static void time_update_task(void *arg) {
   while (1) {
-	vTaskDelay(pdMS_TO_TICKS(1000)); // Wait 1 second
-	
-	// Protect LVGL API calls with mutex
-	_lock_acquire(&lvgl_api_lock);
-	increment_time(); // This will update the display
-	_lock_release(&lvgl_api_lock);
+    vTaskDelay(pdMS_TO_TICKS(1000)); // Wait 1 second
+
+    // Protect LVGL API calls with mutex
+    _lock_acquire(&lvgl_api_lock);
+    increment_time(); // This will update the display
+    _lock_release(&lvgl_api_lock);
   }
 }
 
@@ -57,10 +58,13 @@ void app_main(void) {
   set_time(12, 30, 45); // Set initial time (12:30:45)
   _lock_release(&lvgl_api_lock);
 
+  mod_wifi_init();
+
   // Create LVGL task
   xTaskCreate(example_lvgl_port_task, "LVGL", LVGL_TASK_STACK_SIZE, NULL,
-			  LVGL_TASK_PRIORITY, NULL);
+              LVGL_TASK_PRIORITY, NULL);
 
   // Create time update task
-  xTaskCreate(time_update_task, "TIME", 2048, NULL, LVGL_TASK_PRIORITY - 1, NULL);
+  xTaskCreate(time_update_task, "TIME", 2048, NULL, LVGL_TASK_PRIORITY - 1,
+              NULL);
 }
