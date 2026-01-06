929a7975e1aa3b5b814d25e5cb70bc98d8c24b01
Quang Vinh Dang <dqvinh101@gmail.com>
2026-01-05 16:42:22 -0800

    Architecture planning

---
+++ main/inc/driver/display.h
@@ -0,0 +1,7 @@
+#ifndef __LVGL_DISPLAY_H__
+#define __LVGL_DISPLAY_H__
+
+#include "lvgl.h"
+lv_display_t *display_init(void);
+
+#endif //__LVGL_DISPLAY_H__
---
+++ main/inc/driver/touch.h
@@ -0,0 +1,7 @@
+#ifndef __TOUCH_CONTROLLER_H__
+#define __TOUCH_CONTROLLER_H__
+
+#include "display/lv_display_private.h"
+void touch_controller_init(lv_display_t *display);
+
+#endif //__TOUCH_CONTROLLER_H__
